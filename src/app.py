10837733efd160f15ab86a4609fe87a53e8c4350
Emre Güçlü <68608587+Descless@users.noreply.github.com>
2024-06-09 16:00:31 +0300

    Log-in log-out and New pages
    You can log in
    You can log out
    You can view the new pages
    You have to log in to see the correct page

--- src/app.py
+++ src/app.py
@@ -12,22 +12,32 @@ app.config['MYSQL_DB'] = database.database
 
 mysql = MySQL(app)
 
+is_logged_in = False
+
 @app.route('/')
 def index():
-    return render_template('ana_sayfa.html')
+    if is_logged_in == False:
+        return render_template('ana_sayfa.html')
+    else:
+        return render_template('kullanici_ana_sayfa.html')
+
+
 
 @app.route('/duzelt_sayfasi')
 def correct_page():
     return render_template('duzelt_sayfasi.html')
 
-@app.route('/login', methods=['GET', 'POST'] )
+@app.route('/giris_ekrani', methods=['GET', 'POST'] )
 def login_page():
+    global is_logged_in
     if request.method == 'POST':
         is_true = model.login_page(request, mysql)
         if is_true == False:
            return redirect(url_for('login_page'))
         if is_true == True:
-           return redirect(url_for('correct_page')) 
+           is_logged_in = True
+           print(is_logged_in)
+           return redirect(url_for('index')) 
     else:
         return render_template('giris_ekrani.html')
 
@@ -36,10 +46,6 @@ def sign_in_page():
     if request.method == 'POST':
         model.sign_in(request, mysql)
         return redirect(url_for('login_page'))
-        #if is_true == False:
-           #return redirect(url_for('sign_in_page'))
-       # if is_true == True:
-          # return redirect(url_for('login_page'))
     else:
         return render_template('kayit_ekrani.html')  
 
@@ -54,6 +60,33 @@ def correct_text():
 
     return render_template('duzelt_sayfasi.html',corrected=corrected_text, text=input_text )
     
+
+@app.route('/urunler')
+def products_page():
+    if is_logged_in == False:
+        return render_template('urunler.html')
+    if is_logged_in == True:
+        return render_template('kullanici_urunler.html')
+
+@app.route('/hakkimizda')
+def about_page():
+    if is_logged_in == False:
+        return render_template('hakkimizda.html')
+    if is_logged_in == True:
+        return render_template('kullanici_hakkimizda.html')
+
+@app.route('/iletisim')
+def contact_page():
+    if is_logged_in == False:
+        return render_template('iletisim.html')
+    if is_logged_in == True:
+        return render_template('kullanici_iletisim.html')
+    
+@app.route('/logout')
+def logout():
+    global is_logged_in
+    is_logged_in = False
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
